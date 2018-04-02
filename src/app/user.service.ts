ca6b894b45baf21b902c6a83370b11e9ae406cb5
ndonko tanguy <tndonko@gmail>
2018-04-02 09:27:10 +0200

    service change

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,29 +10,29 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    let url = "http://localhost:8080/api/user/all";
+    let url = "https://userfront.herokuapp.com/api/user/all";
     const httpOptions = {withCredentials:true};
   
     return this.httpClient.get<User[]>(url, httpOptions );
   }
 
   enableUser (username: string) {
-    let url = "http://localhost:8080/api/user/"+username+"/enable";
+    let url = "https://userfront.herokuapp.com/api/user/"+username+"/enable";
     return this.httpClient.get(url, { withCredentials: true });
   }
 
   disableUser (username: string) {
-    let url = "http://localhost:8080/api/user/"+username+"/disable";
+    let url = "https://userfront.herokuapp.com/api/user/"+username+"/disable";
     return this.httpClient.get(url, { withCredentials: true });
   }
 
   getPrimaryTransactionList(username: string):Observable<TransactionList[]> {
-    let url = "http://localhost:8080/api/user/primary/transaction?username="+username;
+    let url = "https://userfront.herokuapp.com/api/user/primary/transaction?username="+username;
    return this.httpClient.get<TransactionList[]>(url, { withCredentials: true });
   }
 
   getSavingsTransactionList(username: string):Observable<TransactionList[]> {
-    let url = "http://localhost:8080/api/user/savings/transaction?username="+username;
+    let url = "https://userfront.herokuapp.com/api/user/savings/transaction?username="+username;
    return this.httpClient.get<TransactionList[]>(url, { withCredentials: true });
   }
 
